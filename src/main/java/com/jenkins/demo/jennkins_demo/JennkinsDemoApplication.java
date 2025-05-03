b8050a5ba2561a327d8ec3049d632087c2fbb8cd
prasadjoshi2385 <prasad.joshi2385@gmail.com>
2025-05-03 16:50:45 +0530

    Add files via upload

---
+++ src/main/java/com/jenkins/demo/jennkins_demo/JennkinsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.demo.jennkins_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JennkinsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JennkinsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jenkins/demo/jennkins_demo/JennkinsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.demo.jennkins_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JennkinsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
